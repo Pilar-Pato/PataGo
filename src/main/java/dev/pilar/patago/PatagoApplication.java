31c0d09bff426bf50a8cec351137bef56c4ed43b
PilarPato <fusionilusion@gmail.com>
2024-12-01 14:06:34 +0100

    Initialize project

---
+++ src/main/java/dev/pilar/patago/PatagoApplication.java
@@ -0,0 +1,13 @@
+package dev.pilar.patago;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatagoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatagoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/pilar/patago/PatagoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.pilar.patago;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatagoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
